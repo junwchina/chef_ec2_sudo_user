9a8153565b44d2041ba0b18ae7cebfa74790329e
Jun <elexjun@gmail.com>
2013-01-10 18:13:32 +0800

    chef user

---
+++ cookbooks/user/recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: user
+# Recipe:: default
+#
+# Copyright 2011, Fletcher Nichol
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+user_account 'focus' do
+  comment   'Focus dev account'
+  home      '/home/focus'
+  shell     '/bin/bash'
+end
+
+## config sudo to run sudo without password
+
+template "/etc/sudoers.d/focus-init-users" do
+  source "sudoers.erb"
+  mode 0440
+  owner "root"
+  group "root"
+  variables(
+    :sudoers_groups => node['sudo']['groups'],
+    :sudoers_users => node['sudo']['users'],
+    :passwordless => node['sudo']['passwordless'],
+  )
+end
