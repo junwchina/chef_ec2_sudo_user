6166fe338c27622d1636c110c7526dd0575fdc50
Jun <elexjun@gmail.com>
2013-01-11 17:53:49 +0800

    add default node config

--- ec2_prepare.sh
+++ ec2_prepare.sh
@@ -11,13 +11,21 @@ source config.sh
 # load tool
 source tool.sh
 
+DEFAULT_NODE_JSON=./nodes/default.json
 
 pub_dns=$(get_pub_dns $1)
+
 
 if [ "$pub_dns" == "" ]; then 
   echo "Please pass a ec2 hostname as the first argument"
   exit $NO_HOST_EXIT
 else
+  # create node.json if it is not exist
+  node_json=./nodes/"$pub_dns".json
+  if [ ! -f  node_json ]; then 
+    cp $DEFAULT_NODE_JSON $node_json
+  fi
+    
   knife prepare -i $KEYPAIR_FILE ubuntu@$pub_dns
 fi
 
